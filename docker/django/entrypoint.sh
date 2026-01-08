42810abae6a0cd11fe5ab3aef05911929c1801fd
Ubaldo Puocci <u.puocci@gmail.com>
2026-01-08 08:26:49 +0100

    first commit

---
+++ docker/django/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+echo "Waiting for postgres..."
+while ! nc -z $DB_HOST ${DB_PORT:-5432}; do
+  sleep 0.5
+done
+echo "PostgreSQL started"
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Starting Gunicorn..."
+exec gunicorn ttstats.wsgi:application \
+  --bind 0.0.0.0:8000 \
+  --workers 3 \
+  --timeout 60 \
+  --access-logfile - \
+  --error-logfile -
